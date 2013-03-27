4d9cf0633334120389f522d6c1fce40651c2bcfe
Md. Adil Akhter <md.adilakhter@gmail.com>
2013-03-27 11:35:48 +0100

    function overloading

---
+++ overloading.fsx
@@ -0,0 +1,19 @@
+﻿  //---------------------overloading-----------------------
+type FunHolder<'Arg, 'Result>() =
+    // F# 3.0
+    //static member val Definition =
+    //    Unchecked.defaultof<'Arg -> 'Result> with get, set
+    // F# 2.0
+    static let mutable definition = Unchecked.defaultof<'Arg -> 'Result>
+    static member Definition with get()  = definition
+                             and  set(x) = definition <- x
+
+FunHolder.Definition <- fun x -> printfn "inside def1"; x * 2
+FunHolder.Definition <- fun (x:string) -> printfn "inside def2";  x + x
+FunHolder.Definition <- fun () -> printfn "inside def3";  (), ()
+
+let twice<'a, 'b> = FunHolder<'a, 'b>.Definition
+
+let x : int         = twice 10  // => 20
+let y : string      = twice "X" // => "XX"
+let z : unit * unit = twice ()  // => (null, null)
